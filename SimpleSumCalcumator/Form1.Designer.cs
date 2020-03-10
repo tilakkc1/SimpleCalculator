f7a396d80ac0650fe9561049a628d772474709a5
Tilak Kc <tilakkc396@gmail.com>
2020-03-10 15:52:17 +0545

    Initial Commit

---
+++ SimpleSumCalcumator/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SimpleSumCalcumator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtn1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtn2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtn3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(69, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Number";
+            // 
+            // txtn1
+            // 
+            this.txtn1.Location = new System.Drawing.Point(202, 44);
+            this.txtn1.Name = "txtn1";
+            this.txtn1.Size = new System.Drawing.Size(277, 20);
+            this.txtn1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(72, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Number";
+            // 
+            // txtn2
+            // 
+            this.txtn2.Location = new System.Drawing.Point(202, 93);
+            this.txtn2.Name = "txtn2";
+            this.txtn2.Size = new System.Drawing.Size(277, 20);
+            this.txtn2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(83, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Sum";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtn3
+            // 
+            this.txtn3.Location = new System.Drawing.Point(202, 159);
+            this.txtn3.Name = "txtn3";
+            this.txtn3.Size = new System.Drawing.Size(277, 20);
+            this.txtn3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 337);
+            this.Controls.Add(this.txtn3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtn2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtn1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtn1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtn2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtn3;
+    }
+}
+
---
+++ SimpleSumCalcumator/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleSumCalcumator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           int num1 = Convert.ToInt32(txtn1.Text);
+           int num2 = Convert.ToInt32(txtn2.Text);
+            txtn3.Text = (num1+num2).ToString();
+        }
+    }
+}
